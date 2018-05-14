acd50004c4c8e17816c9bf6b6ff9ac580144b130
Neza Dukic <neza.djukic@gmail.com>
2018-05-14 19:07:21 +0200

    Initial Commit

---
+++ SkatlicaZdravil/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SkatlicaZdravil
+//
+//  Created by Neza Dukic on 14/05/2018.
+//  Copyright © 2018 Neza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
